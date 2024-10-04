841a257531a0d77bbde516743afbdaa3c6357843
coolboy10086 <140037933+coolboy10086@users.noreply.github.com>
2024-10-04 22:58:23 +0800

    后端初始化工程2222222222222

---
+++ graduation_project_admit1/Back/graduation_project_admit/src/main/java/com/yk/graduation_project_admit/GraduationProjectAdmitApplication.java
@@ -0,0 +1,13 @@
+package com.yk.graduation_project_admit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduationProjectAdmitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraduationProjectAdmitApplication.class, args);
+	}
+
+}
---
+++ graduation_project_admit1/Back/graduation_project_admit/src/test/java/com/yk/graduation_project_admit/GraduationProjectAdmitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yk.graduation_project_admit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduationProjectAdmitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
